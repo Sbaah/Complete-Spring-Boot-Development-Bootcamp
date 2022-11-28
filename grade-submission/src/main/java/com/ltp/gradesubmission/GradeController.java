006eb665a32a4d8a099f5c15b7ac9ead43a0e536
Sbaah <sbaah12@gmail.com>
2022-11-28 16:57:17 -0500

    worked on at work

--- grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -1,14 +1,39 @@
 package com.ltp.gradesubmission;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.ltp.gradesubmission.Grade;
 
 @Controller
-
 public class GradeController {
 
-    @GetMapping("/hello")
-    public String gradeForm() {
-        return "somehtmlview";
+    List<Grade> studentGrades =  Arrays.asList(
+    new Grade ("Harry","Postions","C-" ),
+    new Grade ("Hermione","Artihmancy","A+"),
+    new Grade ("Neville","Chatms","A-" )
+    );
+
+    // @GetMapping("/")
+    // public String home(){
+    //     return "shows";
+    // }
+
+    @GetMapping("/")
+    public String gradeForm(Model model){
+        model.addAttribute("garde", new Grade());
+        return "form";
     }
+
+    @GetMapping("/grades")
+    public String getGrades(Model model) {
+        model.addAttribute("grades", studentGrades);
+        return "grades";
+    }
+
+
+   
 }
